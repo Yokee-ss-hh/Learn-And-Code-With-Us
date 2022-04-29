b7629cb77624fc5d66849eceff4a5cacf133d55c
Yokee-ss-hh <82704862+Yokee-ss-hh@users.noreply.github.com>
2022-04-29 19:59:59 +0530

    Some weird cases in python
    Will add more examples soon!

---
+++ PythonTutorials/WeirdCases.py
@@ -0,0 +1,29 @@
+print("**************************************************")
+# We can literally add boolean and int data types, that means True acts as 1 , and False acts as 0
+
+a = True
+b = 0
+
+print(type(a))
+print(type(b))
+
+print(a+b)
+print(b < a)
+print(False+True)
+
+print(True in [1])
+print(1 < True)
+print(1 < 0 < 1)  # should evaluate as 1<0 and 0<1 =>False and True => False, This is expression chaining
+
+print("****************************************************")
+x = True
+y = False
+print(not x == y)
+# print(x == not y)
+
+print("*********************************************************")
+
+
+
+
+
