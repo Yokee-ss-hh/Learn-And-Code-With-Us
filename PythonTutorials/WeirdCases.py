e899cf073b13eea007d843bda1bff94a9495df68
Yokee-ss-hh <82704862+Yokee-ss-hh@users.noreply.github.com>
2022-05-12 17:28:22 +0530

    Update WeirdCases.py

--- PythonTutorials/WeirdCases.py
+++ PythonTutorials/WeirdCases.py
@@ -23,7 +23,51 @@ print(not x == y)
 
 print("*********************************************************")
 
+print('*****int,float and complex behavior*****')
+# floating variables with nothing except 0's after floating point is always equal to its integer equivalent.
+# objects with same value is equal even they are of different type,That's why == returns True and is returns False.
+int1 = 22
+float1 = 22.0
+float2 = 22.00000000
+complex1 = 22+0j
+complex2 = 22+0.00000j
 
+print('**==**')
+print(int1 == float1)
+print(float1 == float2)
+print(float2 == int1)
+print(float2 == complex1)
+print(complex1 == complex2)
+print(complex2 == int1)
+print('\n')
+print('**is**')
+print(int1 is float1)
+print(float1 is float2)
+print(float2 is int1)
+print(float2 is complex1)
+print(complex1 is complex2)
+print(complex2 is int1)
+
+
+# How this works?
+'''
+1 == 1.0 == 1.000.... == True
+
+In 1.0, after floating point there is no precision so interpreter simply ignores the 0/0's after the floating point.
+We already proved on the top of the file that 1 == True and 0 == False.
+'''
+print("********Some Good Examples**********")
+print(0 == 0.00000000000000)
+print(0 == 0+0j)
+print(2.22 == 2.22+0j)
+print(True == 1+0j)
+print(0 == False)
+print(0.000 == False)
+print(2.000 == (True+1))
+print(0+0j == (1-True))
+print((True+False) == (1.0000+0))
+print((not False) == 1+0j)
+print(3 == ((not 0.0000) + (1+0j) + (not False)))
 
 
 
