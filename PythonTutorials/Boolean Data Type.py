444f537542200f2ddc01856a0217b3ca804449ed
Yokee-ss-hh <82704862+Yokee-ss-hh@users.noreply.github.com>
2022-05-12 17:39:24 +0530

    Update Boolean Data Type.py

--- PythonTutorials/Boolean Data Type.py
+++ PythonTutorials/Boolean Data Type.py
@@ -36,4 +36,32 @@ print(bool(-21))
 print(bool(89.72167))
 print(bool(-12.2))
 print(bool()) # bool with nothing returns False
+print("**********************************************************************")
 
+
+print('*****Boolean on list,tuple,dict,set,frozenset')
+
+print("**list**")
+print(bool([]) == False)
+print(bool(list()) == False)
+print([0][0] == False) # A list with 0 as its only element
+
+print("**tuple**")
+print(bool(()) == False)
+print(bool(tuple()) == False)
+print((0,)[0] == False)
+
+print("**dict**")
+print(bool({}) == False)
+print(bool(dict()) == False)
+print(list({'one':None,'two':2}.values())[0] == None) # None is used to indicate that the variable will be edited with
+                                                      # a new value in future.
+
+print("**set**")
+print(bool(set()) == False)
+print(bool(set({})) == False)
+
+
+print("**frozenset**")
+print(bool(frozenset()) == False)
+print(bool(frozenset({})) == False)
