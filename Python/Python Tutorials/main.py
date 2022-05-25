328a1995c4988d50011ba79669103639548304ea
Yokee-ss-hh <82704862+Yokee-ss-hh@users.noreply.github.com>
2022-05-25 19:36:13 +0530

    Update main.py

--- Python/Python Tutorials/main.py
+++ Python/Python Tutorials/main.py
@@ -1,1 +1,20 @@
+Basic Syntax 
+Variables, Comments, Casting (Implicit and Explicit),snake case, Camel case , pascal case, user input etc....
+In Built Data Types (Numeric, Sequence, Text, Mapping, Set, Boolean, Binary, None. (TIP : Refer files in the Repository)) and Walrus Operator
+Mutable and Immutable differences and main concepts regarding memory usage
+Iterable Expressions (List, Tuple, Set, Dictionary Comprehensions)
+In Built Functions like (map, filter, max, min, sum...........etc, Explore more by searching google or refer Inbuilt functions.py in the Repository)
+Conditional Statements (if,elif,else,switch)
+Looping Statements (for,while)
+Iterators
+Generators, Generator Expressions, Lazy Evaluation vs Strict Evaluation , Normal methods vs generators etc....
+Scope of variables and Functions
+Anonymous Functions (Lambda)
+Closures, Decorators, Decorator Chaining, Decorators with parameters and without parameters
+Dunder or Magic methods
+File Handling
+Exceptions Handling
+Modules and packages 
+Main Important Modules (os,sys,itertools,functools,math,collections,regex,abc,random,datetime,requests,json etc...)
 
+# Add OOPS from here ! after completing above all topics.
